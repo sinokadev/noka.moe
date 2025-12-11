3382c052cb4fc0b3ccea538deb4bfa493060777f
sinokadev <sinoka@sinoka.dev>
2025-12-11 12:28:35 +0900

    으에

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, redirect
+from flask import Flask, render_template, send_file, redirect, request
 import json
 app = Flask(__name__)
 
@@ -7,7 +7,8 @@ import os
 
 load_dotenv()
 
-BASE_PATH = os.getenv("PATH")
+BASE_PATH = os.getenv("BASE_PATH")
+DATA_PATH = os.getenv("DATA_PATH")
 
 def redi_parse(text: str):
     split_text = text.split()
@@ -15,16 +16,22 @@ def redi_parse(text: str):
         return split_text[1]
     else: return None
 
+def is_it_true(value):
+    return value.lower() == 'true'
+
+def safe_redirect(url):
+    if not url.startswith(('http://', 'https://')):
+        url = 'https://' + url
+    return redirect(url)
+
 @app.route("/<just_url>")
 @app.route("/")
 def root(just_url=None):
-    with open("data.json", "r") as file: # read data
+    with open(DATA_PATH, "r") as file: # read data
         data = json.loads(file.read())
     
     if not just_url:
         return render_template("index.html") # defalt
-    
-    file_path = f"{BASE_PATH}{data[just_url]["path"]}"
 
     if just_url not in data:
         return render_template("file_not_found.html"), 404 # not found
@@ -34,6 +41,8 @@ def root(just_url=None):
     with open(file_path, "r") as file: # read file
         parse_result = redi_parse(file.read())
         if parse_result:
-            return redirect(parse_result) # redirect
-    
-    return send_file(file_path) # send file
+            return safe_redirect(parse_result) # redirect
+        
+    is_download = request.args.get('download', default="true", type=str)
+
+    return send_file(file_path, as_attachment=is_it_true(is_download)) # send file
